f34b59705f3c99e38ffdba6c97765bc4ed778a2c
Andrey Sitnik <andrey@sitnik.ru>
2017-05-08 18:09:36 +0200

    Use Jest

---
+++ index.test.js
@@ -0,0 +1,20 @@
+const plugin = require('./');
+
+const postcss = require('postcss');
+
+function run(input, output) {
+    return postcss([ plugin ]).process(input)
+        .then( result => {
+            expect(result.css).toEqual(output);
+            expect(result.warnings().length).toEqual(0);
+        });
+}
+
+it('adds focus selector', () => {
+    return run('a:hover, b {}', 'a:hover, b, a:focus {}');
+});
+
+it('adds focus selector', () => {
+    return run('a:hover, b:hover {}',
+               'a:hover, b:hover, a:focus, b:focus {}');
+});
