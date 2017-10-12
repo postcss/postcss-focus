f388698a22233cfbad332f6eea3a4a5d818ec825
Andrey Sitnik <andrey@sitnik.ru>
2017-10-12 22:46:57 +0300

    Clean up code

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var postcss = require('postcss');
 
+function hasAlready(parent, selector) {
+    return parent.nodes.some(function (i) {
+        return i.type === 'rule' && i.selectors.indexOf(selector) !== -1;
+    });
+}
+
 module.exports = postcss.plugin('postcss-focus', function () {
     return function (css) {
         css.walkRules(function (rule) {
@@ -8,10 +14,7 @@ module.exports = postcss.plugin('postcss-focus', function () {
                 rule.selectors.forEach(function (selector) {
                     if ( selector.indexOf(':hover') !== -1 ) {
                         var replaced = selector.replace(/:hover/g, ':focus');
-                        if (!rule.parent.nodes.some(function (i) {
-                            return i.type === 'rule' &&
-                                   i.selectors.indexOf(replaced) !== -1;
-                        })) {
+                        if ( !hasAlready(rule.parent, replaced) ) {
                             focuses.push(replaced);
                         }
                     }
