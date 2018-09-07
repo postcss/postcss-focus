8af78542e0bcb3d4017c9fa75c93033fd0ffb84a
Andrey Sitnik <andrey@sitnik.ru>
2018-09-07 16:06:25 +0300

    Update dependencies

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-var postcss = require('postcss');
+var postcss = require('postcss')
 
-function hasAlready(parent, selector) {
-    return parent.nodes.some(function (i) {
-        return i.type === 'rule' && i.selectors.indexOf(selector) !== -1;
-    });
+function hasAlready (parent, selector) {
+  return parent.nodes.some(function (i) {
+    return i.type === 'rule' && i.selectors.indexOf(selector) !== -1
+  })
 }
 
 module.exports = postcss.plugin('postcss-focus', function () {
-    return function (css) {
-        css.walkRules(function (rule) {
-            if ( rule.selector.indexOf(':hover') !== -1 ) {
-                var focuses = [];
-                rule.selectors.forEach(function (selector) {
-                    if ( selector.indexOf(':hover') !== -1 ) {
-                        var replaced = selector.replace(/:hover/g, ':focus');
-                        if ( !hasAlready(rule.parent, replaced) ) {
-                            focuses.push(replaced);
-                        }
-                    }
-                });
-                if ( focuses.length ) {
-                    rule.selectors = rule.selectors.concat(focuses);
-                }
+  return function (css) {
+    css.walkRules(function (rule) {
+      if (rule.selector.indexOf(':hover') !== -1) {
+        var focuses = []
+        rule.selectors.forEach(function (selector) {
+          if (selector.indexOf(':hover') !== -1) {
+            var replaced = selector.replace(/:hover/g, ':focus')
+            if (!hasAlready(rule.parent, replaced)) {
+              focuses.push(replaced)
             }
-        });
-    };
-});
+          }
+        })
+        if (focuses.length) {
+          rule.selectors = rule.selectors.concat(focuses)
+        }
+      }
+    })
+  }
+})
--- index.test.js
+++ index.test.js
@@ -1,34 +1,35 @@
-const plugin = require('./');
+var plugin = require('./')
 
-const postcss = require('postcss');
+var postcss = require('postcss')
 
-function run(input, output) {
-    return postcss([ plugin ]).process(input).then( result => {
-        expect(result.css).toEqual(output);
-        expect(result.warnings().length).toEqual(0);
-    });
+function run (input, output) {
+  return postcss([plugin]).process(input, { from: undefined })
+    .then(function (result) {
+      expect(result.css).toEqual(output)
+      expect(result.warnings()).toHaveLength(0)
+    })
 }
 
-it('adds focus selector', () => {
-    return run('a:hover, b {}', 'a:hover, b, a:focus {}');
-});
+it('adds focus selector', function () {
+  return run('a:hover, b {}', 'a:hover, b, a:focus {}')
+})
 
-it('adds focus selector', () => {
-    return run(
-        'a:hover, b:hover {}',
-        'a:hover, b:hover, a:focus, b:focus {}'
-    );
-});
+it('adds focus selectors', function () {
+  return run(
+    'a:hover, b:hover {}',
+    'a:hover, b:hover, a:focus, b:focus {}'
+  )
+})
 
-it('ignores hover selector because of focus', () => {
-    return run(
-        '.foo:hover {} .foo:focus {} ' +
-            'a:hover, b:hover {} ' +
-            'b:focus {} ' +
-            '@media { b:hover {} }',
-        '.foo:hover {} .foo:focus {} ' +
-            'a:hover, b:hover, a:focus {} ' +
-            'b:focus {} ' +
-            '@media { b:hover, b:focus {} }'
-    );
-});
+it('ignores hover selector because of focus', function () {
+  return run(
+    '.foo:hover {} .foo:focus {} ' +
+      'a:hover, b:hover {} ' +
+      'b:focus {} ' +
+      '@media { b:hover {} }',
+    '.foo:hover {} .foo:focus {} ' +
+      'a:hover, b:hover, a:focus {} ' +
+      'b:focus {} ' +
+      '@media { b:hover, b:focus {} }'
+  )
+})
